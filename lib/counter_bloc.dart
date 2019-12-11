dbf92eeb2fdcf826dcb601fc245327d8482cf76b
Alex Subota <as.subota@gmail.com>
2019-12-11 09:04:14 +0200

    Work on bloc tutorial

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,4 +1,24 @@
 import 'dart:async';
 import 'package:learn_flutter/counter_event.dart';
 
-class CounterBloc {}
+class CounterBloc {
+  int _counter = 0;
+
+  final _counterStreamController = StreamController<int>();
+  StreamSink<int> get _inCounter => _counterStreamController.sink;
+  Stream<int> get counter => _counterStreamController.stream;
+
+  final _counterEventController = StreamController<CounterEvent>();
+  Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventsToState);
+  }
+
+  void _mapEventsToState(CounterEvent event) {
+    if (event is IncrementEvent) _counter++;
+    if (event is DecrementEvent) _counter--;
+
+    _inCounter.add(_counter);
+  }
+}
